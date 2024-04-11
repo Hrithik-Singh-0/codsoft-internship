30bd504c4acba9fe487c9d81ff351866777a1152
Hrithik Singh <115973059+Hrithik-Singh-0@users.noreply.github.com>
2024-04-11 13:06:30 +0530

    Add files via upload

---
+++ codsoft/urls.py
@@ -0,0 +1,28 @@
+"""codsoft URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.homepage),
+    path('rps_game/', include('rock_paper_scissors.urls')),
+    path('pass_gen/', include('pass_gen.urls')),
+    path('calculator/', include('basic_calculator.urls')),
+    path('todolist/', include('to_do_list.urls')),
+    path('contacts/', include('contact_list.urls')),
+]
